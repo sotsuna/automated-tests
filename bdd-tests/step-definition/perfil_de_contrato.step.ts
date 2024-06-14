e761638a327658e1536887d87395e27f45298dd6
sotsuna <mkist202@gmail.com>
2024-06-14 12:00:47 -0300

    changed how xpath works

--- bdd-tests/step-definition/perfil_de_contrato.step.ts
+++ bdd-tests/step-definition/perfil_de_contrato.step.ts
@@ -1,21 +1,34 @@
-import { Given, When, Then } from '@cucumber/cucumber';
+import { Given } from '@cucumber/cucumber';
 import { ICustomWorld } from '../support/custom-world';
 
-
-Given('que eu estou logado no sistema', async function (this: ICustomWorld) {
-  await this.page!.goto('http://172.0.0.234:8313/mk/login/?sys=MK0');
-  await this.page!.fill('input[name="user"]', 'marco.kist');
-  await this.page!.fill('input[name="password"]', 'Mart0123!');
+Given('que eu estou logado no sistema', { timeout: 2 * 5000 }, async function (this: ICustomWorld) {
+  await this.page!.goto('http://172.0.0.234:8313/mk/login');
+  await this.page!.fill('input[name="user"]', '');
+  await this.page!.fill('input[name="password"]', '');
   await this.page!.click('button[name="user"]');
 });
 
-Given('estou na tela inicial do sistema', {timeout: 2 * 5000}, async function (this: ICustomWorld) {
-  const mainFrame = await this.page!.frame({url: 'http://172.0.0.234:8313/mk/form.jsp?sys=MK0&action=openform&formID=8267&align=0&mode=-1&goto=-1&filter=&scrolling=False&firstLoad=true'});
-  if (mainFrame) {
-    await mainFrame.waitForSelector('title');
-  }
-});
+Given('estou na tela inicial do sistema', { timeout: 2 * 5000 }, async function (this: ICustomWorld) {
+  await new Promise(resolve => setTimeout(resolve, 3000));
 
-Given('eu clico na moeda de configuração', async function (this: ICustomWorld) {
-  await this.page!.locator('///html/body/form[1]/div/div[3]/div[1]/div/ul/li[9]/a').click();
+  try {
+    this.page?.frameLocator(
+      '//html/frameset/frame'
+    );
+  } catch (error: any) {
+    console.log(error);
+  }
+})
+
+Given('eu clico na moeda de configuração', { timeout: 5000 * 3 }, async function (this: ICustomWorld) {
+  try {
+    const configButton = this.page?.frameLocator(
+      '//html/frameset/frame'
+    ).frameLocator('//html/body/iframe').locator('//html/body/form[1]/div/div[3]/div[1]/div/ul/li[9]/a');
+    await configButton?.click();
+    console.log(configButton);
+  } catch (error: any) {
+    console.log(error);
+  };
+
 });

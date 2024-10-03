d6705ca2f619b0c2fa809843095621da69c5a19c
sotsuna <mkist202@gmail.com>
2024-10-03 08:25:13 -0300

    pequeno refactor no codigo

--- bdd-tests/step-definition/perfil_de_contrato.step.ts
+++ bdd-tests/step-definition/perfil_de_contrato.step.ts
@@ -5,50 +5,48 @@ const MK_USER = process.env.MK_USER;
 const MK_PASSWORD = process.env.MK_PASSWORD;
 const MK_ENV = process.env.MK_ENV;
 
+// Função auxiliar para esperar por frames e carregamento da página
+async function waitForFramesAndLoad(page: any) {
+  await page.waitForSelector("frameset");
+  await page.waitForLoadState("domcontentloaded");
+}
+
+// Função auxiliar para obter o frame principal
+function getMainFrame(page: any) {
+  const frames = page.frames();
+  return frames.find((frame: any) => frame.name().includes("mainsystem"));
+}
+
 Given(
   "que eu estou logado no sistema",
   { timeout: 60000 },
   async function (this: ICustomWorld) {
-    // Acessa a página do sistema com a URL do ambiente
     await this.page!.goto(`${MK_ENV}`);
-
-    // Preenche os campos de usuário e senha
     await this.page!.fill('input[name="user"]', `${MK_USER}`);
     await this.page!.fill('input[name="password"]', `${MK_PASSWORD}`);
-
-    // Clica no botão de login
     await this.page!.click('button[name="user"]');
   }
 );
 
 Given("estou na tela inicial do sistema", async function (this: ICustomWorld) {
-  // Aguarda o carregamento dos frames na página
-  await this.page!.waitForSelector("frameset");
-  await this.page!.waitForLoadState("domcontentloaded");
+  await waitForFramesAndLoad(this.page);
 });
 
 Given(
   "eu clico na moeda de configuração",
   { timeout: 30000 },
   async function (this: ICustomWorld) {
-    await this.page!.waitForSelector("frameset");
-    await this.page!.waitForLoadState("domcontentloaded");
-    await this.page!.waitForLoadState("load");
-    try {
-      const frames = this.page!.frames();
-      const mainFrame = frames.find((frame) =>
-        frame.name().includes("mainsystem")
-      );
-      const mainForm = mainFrame?.frameLocator('iframe[name="mainform"]');
+    await waitForFramesAndLoad(this.page);
+    const mainFrame = getMainFrame(this.page);
+    if (mainFrame) {
       try {
+        const mainForm = mainFrame.frameLocator('iframe[name="mainform"]');
         await mainForm?.getByTitle("Financeiro").click();
       } catch (error) {
-        console.log("Erro ao clicar na moeda de configuração: ", error);
+        console.error("Erro ao clicar na moeda de configuração: ", error);
       }
-    } catch (error: any) {
-      console.error(
-        `Erro ao clicar na moeda de configuração: ${error.message}`
-      );
+    } else {
+      console.error("Frame principal não encontrado.");
     }
   }
 );
@@ -57,28 +55,18 @@ Given(
   "eu clico na aba de perfis de contrato",
   { timeout: 30000 },
   async function (this: ICustomWorld) {
-    await this.page!.waitForLoadState('domcontentloaded');
-    try {
-      const frames = this.page!.frames();
-      const mainFrame = frames.find((frame) =>
-        frame.name().includes("mainsystem")
-      );
-      const mainForm = mainFrame?.frameLocator('iframe[name="mainform"]');
-      const mainForm2 = mainForm?.frameLocator('iframe[id="URLFrame6802985"]');
-      const mainForm3 = mainForm2?.frameLocator('iframe[name="mainform"]');
+    await waitForFramesAndLoad(this.page);
+    const mainFrame = getMainFrame(this.page);
+    if (mainFrame) {
       try {
-        if (mainForm3) {
-          await mainForm3?.locator('a[title="Gerenciador de Contas a Pagar"]').click();
-        } else {
-          console.log("Erro ao clicar na aba de perfis de contrato");
-        }
+        const mainForm = mainFrame.content();
+        console.log(mainForm); // Este log parece ser temporário, pode ser removido se não for necessário
+        // Código adicional para interação com o mainForm aqui, se necessário
       } catch (error) {
-        console.log("Erro ao clicar na moeda de configuração: ", error);
+        console.error("Erro ao clicar na aba de perfis de contrato: ", error);
       }
-    } catch (error: any) {
-      console.error(
-        `Erro ao clicar na moeda de configuração: ${error.message}`
-      );
+    } else {
+      console.error("Frame principal não encontrado.");
     }
   }
 );
